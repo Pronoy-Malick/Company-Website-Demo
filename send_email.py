6e0e9ee48253a46ce2409083677a105f9c9b5fdd
Pronoy Malick <malickpronoy@gmail.com>
2025-04-15 11:43:11 +0530

    Added the password to environment variable

--- send_email.py
+++ send_email.py
@@ -1,4 +1,5 @@
 import smtplib,ssl
+import os
 
 
 def send_email(message):
@@ -6,7 +7,7 @@ def send_email(message):
     port = 465
 
     username = "pronoymalick2004@gmail.com"
-    password = "hhqc cjvd vvkd yvof"
+    password = os.getenv("PASSWORD")
 
     receiver = "pronoymalick2004@gmail.com"
     context = ssl.create_default_context()
